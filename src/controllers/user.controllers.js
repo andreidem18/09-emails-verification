1aa20d0ac4aa0b9432313958f58087cb6f6c5251
Andres Mendoza <andres.david.mm@hotmail.com>
2023-11-10 22:42:58 -0500

    added verification and login

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,6 +2,8 @@ const catchError = require('../utils/catchError');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const sendEmail = require('../utils/sendEmail');
+const EmailCode = require('../models/EmailCode');
+const jwt = require('jsonwebtoken');
 
 const getAll = catchError(async(req, res) => {
     const results = await User.findAll();
@@ -21,6 +23,11 @@ const create = catchError(async(req, res) => {
     });
 
     const code = require('crypto').randomBytes(32).toString("hex")
+
+    await EmailCode.create({
+        code: code,
+        userId: result.id,
+    });
 
     const link = `${frontBaseUrl}/auth/verify_email/${code}`
 
@@ -61,10 +68,49 @@ const update = catchError(async(req, res) => {
     return res.json(result[1][0]);
 });
 
+// /users/verify/:code
+const verifyCode = catchError(async(req, res) => {
+    const { code } = req.params;
+    const emailCode = await EmailCode.findOne({ where: {code} });
+    if (!emailCode) return res.status(401).json({ message: "Code not found" });
+    const user = await User.findByPk(emailCode.userId);
+    user.isVerified = true;
+    await user.save();
+    await emailCode.destroy();
+    return res.json(user);
+});
+
+const login = catchError(async(req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) return res.status(401).json({ message: 'Invalid credentials'});
+    if (!user.isVerified) return res.status(401).json({ message: 'User not verified'});
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) return res.status(401).json({ message: 'Invalid credentials'});
+
+    const token = jwt.sign(
+        { user },
+        process.env.TOKEN_SECRET,
+        { expiresIn: '1d' },
+    );
+    return res.json({ user, token });
+
+})
+
+
+const getLoggedUser = catchError(async(req, res) => {
+    const user = req.user;
+    return res.json(user);
+});
+
+
 module.exports = {
     getAll,
     create,
     getOne,
     remove,
-    update
+    update,
+    verifyCode,
+    login,
+    getLoggedUser,
 }
